1513cc9bf8911197dc168e531977ffcf93a51886
higkoo <higkoohk@gmail.com>
2017-11-05 01:49:24 +0800

    Fix a Bug
    Replace ping by curl.

--- index.php
+++ index.php
@@ -4,16 +4,16 @@ $system = ini_get('system');
 $win  = is_bool($system);
 $count = 1;
 
-$host[1] = "www.bilibili.com";
-$host[2] = "www.baidu.com";
-$host[3] = "www.google.com";
-$host[4] = "cn.bing.com";
-$host[5] = "www.iqiyi.com";
-$host[6] = "www.12306.cn";
-$host[7] = "github.com";
-$host[8] = "www.youku.com";
-$host[9] = "news.163.com";
-$host[10] = "www.kingsoft.com";
+$host[1] = "http://www.bilibili.com/";
+$host[2] = "http://www.baidu.com/";
+$host[3] = "http://www.google.com/";
+$host[4] = "http://cn.bing.com/";
+$host[5] = "http://www.iqiyi.com/";
+$host[6] = "http://www.12306.cn/";
+$host[7] = "http://github.com/";
+$host[8] = "http://www.youku.com/";
+$host[9] = "http://news.163.com/";
+$host[10] = "http://www.kingsoft.com/";
 
 $services[1] = "bilibili";
 $services[2] = "baidu";
@@ -30,27 +30,35 @@ $services[10] = "kingsoft";
 echo "<table border=\"0\" align=\"center\">";
 foreach ($host as $value) 
 {
-	 $counter = $count + 1;
-	  echo "<tr><td width=120>$value</td>"; 
-      echo '<body bgcolor="#FFFFFF" text="#000000"></body>';       
-      //check target IP or domain
-	  $pingreply = exec("ping -n $count $value");
-	  if ( substr($pingreply, -2) == 'ms')
+	
+	$counter = $counter + 1;
+	echo "<tr><td width=120>$value</td>"; 
+	echo '<body bgcolor="#FFFFFF" text="#000000"></body>';      
+	
+  	$ch = curl_init($value);
+	@curl_setopt($ch, CURLOPT_HEADER         ,true); 
+	@curl_setopt($ch, CURLOPT_NOBODY         ,true); 
+	@curl_setopt($ch, CURLOPT_RETURNTRANSFER ,true); 
+	curl_exec($ch);
+	
+	  if ( !curl_errno($ch))
   		{
 			#echo "<td width=60><strong><font color='#006600'>UP</font></strong></td>";
-			echo "<td width=60><img src='up.png'></td>";
+			echo "<td width=60><img src='http://icons.iconarchive.com/icons/gakuseisean/ivista/128/Good-or-Tick-icon.png'></td>";
 			echo "<td width=230>". $services[$counter] . "</td>";
 		    echo "<td>Reply Speed ";
-		    echo substr($pingreply, -13);
+		  $info = curl_getinfo($ch);
+		    echo $info['total_time'];
 		}
 	  else 
 		{
 			#echo "<td width=60><strong><font color='#990000'>DOWN</font></strong></td>";
-			echo "<td width=60><img src='down.jpg'></td>";
+			echo "<td width=60><img src='https://upload.wikimedia.org/wikipedia/commons/6/61/Crystal_128_error.png'></td>";
 			echo "<td width=230>". $services[$counter] . "</td>";
 		    echo "<td>";
 			echo "Timeout...";
 		}
+	curl_close($ch);
 }
 echo "</td></tr></table>";
 ?>
